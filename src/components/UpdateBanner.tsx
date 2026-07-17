a079c6135b6ffdc3806a9d22ded224227e73d176
Aether Builder <aether@github.com>
2026-07-17 15:30:27 +0000

    Fix import path in UpdateBanner

--- src/components/UpdateBanner.tsx
+++ src/components/UpdateBanner.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { APP_VERSION, checkForUpdates } from './updateChecker';
+import { APP_VERSION, checkForUpdates } from '../updateChecker';
 import { Button, Card } from './components';
 import { Download, X, RefreshCw, Package } from 'lucide-react';
 
