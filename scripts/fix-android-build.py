38bd3e8ec9a8c8633ec11bb81e2707a6b3d3287a
Aether Builder <aether@github.com>
2026-07-18 17:10:22 +0000

    v0.1.25: fix gradle build - properly inject signing config inside android block

--- scripts/fix-android-build.py
+++ scripts/fix-android-build.py
@@ -17,33 +17,43 @@ content = re.sub(r'.*nura-debug\.keystore.*\n?', '', content)
 content = re.sub(r".*nura123.*\n?", '', content)
 content = re.sub(r'.*kotlin-stdlib-jdk.*\n?', '', content)
 
-# Add Kotlin resolution strategy
-resolution = """
+# Inject signing config INSIDE android { block
+signing = (
+    '    signingConfigs {\n'
+    '        debug {\n'
+    '            storeFile file(\'../../keystore/nura-debug.keystore\')\n'
+    '            storePassword \'nura123\'\n'
+    '            keyAlias \'nura\'\n'
+    '            keyPassword \'nura123\'\n'
+    '        }\n'
+    '    }\n\n'
+)
 
-configurations.all {
-    resolutionStrategy {
-        force 'org.jetbrains.kotlin:kotlin-stdlib:1.9.22'
-        force 'org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.22'
-        force 'org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22'
-    }
-}
-"""
-content += resolution
-
-# Add signing config INSIDE android block
-signing = """    signingConfigs {
-        debug {
-            storeFile file('../../keystore/nura-debug.keystore')
-            storePassword 'nura123'
-            keyAlias 'nura'
-            keyPassword 'nura123'
-        }
-    }
-"""
+# Replace first occurrence of 'android {'
 content = content.replace('android {', 'android {\n' + signing, 1)
+
+# Add Kotlin resolution strategy at end
+resolution = (
+    '\n\nconfigurations.all {\n'
+    '    resolutionStrategy {\n'
+    "        force 'org.jetbrains.kotlin:kotlin-stdlib:1.9.22'\n"
+    "        force 'org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.22'\n"
+    "        force 'org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22'\n"
+    '    }\n'
+    '}\n'
+)
+content += resolution
 
 with open(GRADLE_FILE, 'w') as f:
     f.write(content)
 
 print(f'Fixed {GRADLE_FILE}')
-print('Added: signingConfigs + resolutionStrategy')
+sc_count = content.count('signingConfigs')
+print(f'signingConfigs references: {sc_count}')
+
+idx_android = content.index('android {')
+idx_sc = content.index('signingConfigs')
+if idx_sc > idx_android:
+    print('OK: signingConfigs is INSIDE android block')
+else:
+    print('WARNING: signingConfigs might be in wrong position')
